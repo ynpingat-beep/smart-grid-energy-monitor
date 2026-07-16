63ab4a04b8383ce5f50d13c390b31a3a4e4c8041
Yash Pingat <ynpingat@gmail.com>
2026-07-16 23:56:34 +0530

    feat: configure SQLAlchemy database connection

---
+++ smart-grid-energy-monitor/app/db/database.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+from app.core.config import DATABASE_URL
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()
