c35482141262eb06f339f397d48af165d2a4f5e5
Yash Pingat <ynpingat@gmail.com>
2026-08-02 23:28:32 +0530

    feat: improve energy reading API workflow

--- smart-grid-energy-monitor/app/routers/reading.py
+++ smart-grid-energy-monitor/app/routers/reading.py
@@ -5,6 +5,9 @@ from app.db.database import SessionLocal
 from app.schemas import EnergyReadingCreate
 from app.crud import create_reading, get_readings
 
+from app.routers.websocket import broadcast_alert
+import asyncio
+
 router = APIRouter(prefix="/readings", tags=["Energy Readings"])
 
 
@@ -18,7 +21,31 @@ def get_db():
 
 @router.post("/")
 def add_reading(reading: EnergyReadingCreate, db: Session = Depends(get_db)):
-    return create_reading(db, reading)
+
+    new_reading = create_reading(db, reading)
+
+    try:
+
+        loop = asyncio.get_event_loop()
+
+        if new_reading.voltage > 235:
+            loop.create_task(
+                broadcast_alert(
+                    f"🚨 High Voltage Alert ({new_reading.voltage} V)"
+                )
+            )
+
+        if new_reading.power > 1250:
+            loop.create_task(
+                broadcast_alert(
+                    f"⚡ High Power Alert ({new_reading.power} W)"
+                )
+            )
+
+    except Exception as e:
+        print("Alert Error:", e)
+
+        return new_reading
 
 
 @router.get("/")
